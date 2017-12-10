0e70aaf960294d299dd62e207ce9b19ec87c77a1
zybeasy <zybeasy@163.com>
2017-12-10 23:40:50 +0800

    Update python描述符.py

--- python描述符.py
+++ python描述符.py
@@ -20,9 +20,23 @@ class Data(object):
 
 data = Data()
 print Data.desc, "\n", data.desc
-
 data.desc = 1000
 print Data.desc, "\n", data.desc
-
 Data.desc = 9999
 print Data.desc, "\n", data.desc
+"""Output
+None <class '__main__.Data'> GET
+0
+<__main__.Data object at 0x102997a50> <class '__main__.Data'> GET
+0
+<__main__.Data object at 0x102997a50> SET
+None <class '__main__.Data'> GET
+1000
+<__main__.Data object at 0x102997a50> <class '__main__.Data'> GET
+1000
+9999
+9999
+说明：最后Data.desc = 9999,因为使用类设置的，所以没有走入描述符的__set__，然后desc的类型就不是Desc，
+最后的print Data.desc, "\n", data.desc也就不会走入Desc的描述符__get__流程。
+"""
+
